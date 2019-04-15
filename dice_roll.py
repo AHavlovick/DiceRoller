b75bf3997a7aefadab46edfde9c2ab11e6a54ed5
AHavlovick <anthony.havlovick@gmail.com>
2019-04-15 17:26:08 -0500

    Added input Validation Class

--- dice_roll.py
+++ dice_roll.py
@@ -1,5 +1,27 @@
 from random import randint
 
+class Diestate:
+  def __init__(self, value):
+    self.value = value
+
+  def validinteger(self):
+    try:
+      test = int(self.value)
+    except ValueError:
+      return False
+    else:
+      if test < 1:
+        return False
+      else:
+        return True
+
+  def validface(self):
+    facetypes = [4, 6, 8, 10, 12, 20]
+    if self.value in facetypes:
+      return True
+    else:
+      return False
+
 class Dice:
 	def __init__(self, faces):
 		self.faces = faces
--- test_dice_roll.py
+++ test_dice_roll.py
@@ -20,3 +20,39 @@ class TestDiceRoller(unittest.TestCase):
     d = dice_roll.DiceRoller(dice_roll.Dice(6))
     t = d.roll(4)
     self.assertTrue(len(t) == 4)
+
+class TestInvalidInputLetter(unittest.TestCase):
+  """
+  Test roll with non-numerical character
+  """
+  def test_input_letter(self):
+    d = dice_roll.Diestate('a')
+    t = d.validinteger()
+    self.assertTrue(t == False)
+
+class TestInvalidInputFloat(unittest.TestCase):
+  """
+  Test input with Float
+  """
+  def test_input_float(self):
+    d = dice_roll.Diestate(5.6)
+    t = d.validinteger()
+    self.assertTrue(t == False)
+
+class TestInvalidInputNegative(unittest.TestCase):
+  """
+  Test input with negative number
+  """
+  def test_input_negative(self):
+    d = dice_roll.Diestate(-4)
+    t = d.validinteger()
+    self.assertTrue(t == False)
+
+class TestInvalidFaceSelction(unittest.TestCase):
+  """
+  Test face input in acceptable facetypes
+  """
+  def test_invalid_face(self):
+    d = dice_roll.Diestate(7)
+    t = d.validface()
+    self.assertTrue(t == False)
