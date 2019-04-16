b2afccf2b2fa4c31fe18a94b5b2001a06130c605
AHavlovick <anthony.havlovick@gmail.com>
2019-04-16 16:04:00 -0500

    Added User Input and looping logic

--- dice_roll.py
+++ dice_roll.py
@@ -17,7 +17,7 @@ class Diestate:
 
   def validface(self):
     facetypes = [4, 6, 8, 10, 12, 20]
-    if self.value in facetypes:
+    if int(self.value) in facetypes:
       return True
     else:
       return False
@@ -38,3 +38,42 @@ class DiceRoller:
     for x in range(count):
       rolls.append(self.dice.roll())
     return rolls
+
+class UserInput:
+  def __init__(self):
+    self.invalidnum = 'That is not a valid number'
+
+  def faceinput(self):
+    while True:
+      faces = input('How many Faces?')
+      ds = Diestate(faces)
+      if ds.validinteger() == False:
+        print(self.invalidnum)
+      elif ds.validface() == False:
+        print('Choose 4, 6, 8, 10, 12, or 20')
+      else:
+        return faces
+    return faces
+
+  def diceinput(self):
+    while True:
+      dice = input('How many Dice?')
+      ds = Diestate(dice)
+      if ds.validinteger() == False:
+        print(self.invalidnum)
+      else:
+        return dice
+    return dice
+
+reroll = 'yes'
+while reroll == 'yes' or reroll == 'y':
+  ui = UserInput()
+  faces = int(ui.faceinput())
+  dice = int(ui.diceinput())
+  result = DiceRoller(Dice(faces)).roll(dice)
+  print(*result, sep=' + ')
+  print(sum(result))
+
+  reroll = input('roll again? (yes/no)')
+  if reroll == 'no' or reroll == 'n':
+    exit()
